0fdf73e209ff84cb19b05af210694d6db76618c4
Andonys24 <andonihernandez1990@gmail.com>
2024-08-09 21:18:32 -0600

    - Mostrando en el carrito el curso añadido - Mostrando el resto de informacion del curso

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@ const carrito = document.querySelector("#carrito"),
 	contenedorCarrito = document.querySelector("#lista-carrito tbody"),
 	vaciarCarrito = document.querySelector("#vaciar-carrito");
 
+let articulosCarrito = [];
+
 cargarEventListeners();
+
 function cargarEventListeners() {
 	// cuando agregas un curso presionando "Agregar al Carrito"
 	listaCursos.addEventListener("click", agregarCurso);
@@ -20,7 +23,6 @@ function agregarCurso(e) {
 
 // lee el contenido del HTMl al que le dimos click y extrae la informacion del curso
 function leerDatosCurso(curso) {
-	console.log(curso);
 	// Crear un objeto con el contenido del curso actual
 	const infoCurso = {
 		imagen: curso.querySelector("img").src,
@@ -30,5 +32,65 @@ function leerDatosCurso(curso) {
 		cantidad: 1,
 	};
 
-	console.log(infoCurso);
+	// Agrega elementos al arreglo de carrito
+	articulosCarrito = [...articulosCarrito, infoCurso];
+	carritoHTML();
+}
+
+// Muetsra el carrito de compras en el HTML
+function carritoHTML() {
+	// Limpiar el HTML
+	limpiarHTML();
+
+	articulosCarrito.forEach((curso) => {
+		const { imagen, titulo, precio, cantidad, id } = curso;
+		const row = document.createElement("tr");
+
+		// Crear y agregar la celda de la imagen
+		const imgCell = document.createElement("td");
+		const img = document.createElement("img");
+		img.src = imagen;
+		img.width = 100;
+		imgCell.appendChild(img);
+		row.appendChild(imgCell);
+
+		// Crear y agregar la celda del título
+		const titleCell = document.createElement("td");
+		titleCell.textContent = titulo;
+		row.appendChild(titleCell);
+
+		// Crear y agregar la celda del precio
+		const priceCell = document.createElement("td");
+		priceCell.textContent = precio;
+		row.appendChild(priceCell);
+
+		// Crear y agregar la celda de la cantidad
+		const quantityCell = document.createElement("td");
+		quantityCell.textContent = cantidad;
+		row.appendChild(quantityCell);
+
+		// Crear y agregar la celda del enlace de borrar
+		const deleteCell = document.createElement("td");
+		const deleteLink = document.createElement("a");
+		deleteLink.href = "#";
+		deleteLink.className = "borrar-curso";
+		deleteLink.dataset.id = id;
+		deleteLink.textContent = " X ";
+		deleteCell.appendChild(deleteLink);
+		row.appendChild(deleteCell);
+
+		// agregar el HTML del carrito en el tbody
+		contenedorCarrito.appendChild(row);
+	});
+}
+
+// Eliminar los cursos del tbody
+function limpiarHTML() {
+	// Forma lenta
+	// contenedorCarrito.innerHTML = "";
+
+	// Forma rapida
+	while (contenedorCarrito.firstChild) {
+		contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+	}
 }
